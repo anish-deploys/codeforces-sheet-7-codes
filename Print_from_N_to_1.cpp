b640f1c115823cac0c8f9cfdbdf95ff2af6b0dd3
Anish Bakshi <117637218+anish-deploys@users.noreply.github.com>
2025-04-06 14:55:09 +0530

    Add files via upload

---
+++ Print_from_N_to_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<cmath>
+#include<iomanip>
+#define ll long long
+using namespace std;
+
+int rec(int n)
+{
+    if (n == 0) return 0;
+    cout << n;
+
+    if (n != 1) cout << " ";
+    return rec(n-1);
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    rec(x);
+
+    return 0;
+}
